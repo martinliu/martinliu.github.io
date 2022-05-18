1602b1625f4fcca3ed0ed38a9e2aad1ef30b1269
Martin Liu <liuzh66@gmail.com>
2022-05-18 23:42:41 +0800

    change js angent file

--- static/js/add-on.js
+++ static/js/add-on.js
@@ -4,3 +4,13 @@
   is an official part of the theme (ex. Pull Requests) should be included in main.js
   and follow the formatting and style given.
 */
+<script src="/js/elastic-apm-rum.umd.min.js"></script>
+<script>
+  elasticApm.init({
+    serviceName: 'Martin-Blog-Cloudflare',
+    environment: 'production',
+    serviceVersion: '1.6.4',
+    breakdownMetrics: 'true',
+    serverUrl: 'https://b1c7fb0e25894d4db3cabb678546c24f.apm.ap-east-1.aws.elastic-cloud.com:443',
+  })
+</script>
